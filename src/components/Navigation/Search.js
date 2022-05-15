0b260e68f3c63454d927130facfed7d03247d741
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-05-15 21:07:05 +0530

    Enter to Search Add

--- src/components/Navigation/Search.js
+++ src/components/Navigation/Search.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { FiSearch } from "react-icons/fi";
 import { CgClose } from "react-icons/cg";
@@ -9,6 +9,15 @@ import useWindowDimensions from "../../hooks/useWindowDimensions";
 function Search({ isActive, setIsActive }) {
   const [title, setTitle] = useState("");
   const { width, height } = useWindowDimensions();
+  const navigate = useNavigate();
+
+  function searchEnter() {
+    if (title !== "") {
+      setIsActive(false);
+      navigate("/search/" + title);
+    }
+  }
+
   return (
     <Wrapper>
       <CloseButton>
@@ -71,16 +80,24 @@ function Search({ isActive, setIsActive }) {
               onChange={(e) => {
                 setTitle(e.target.value);
               }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  searchEnter();
+                }
+              }}
             />
           </div>
-          <SearchButton
-            to={"/search/" + title}
-            onClick={(e) => {
-              setIsActive(false);
-            }}
-          >
-            Search
-          </SearchButton>
+          {title !== "" && (
+            <SearchButton
+              to={"/search/" + title}
+              onClick={(e) => {
+                setIsActive(false);
+              }}
+            >
+              Search
+            </SearchButton>
+          )}
+          {title === "" && <button>Search</button>}
         </div>
       </Content>
     </Wrapper>
@@ -136,6 +153,26 @@ const Content = styled.div`
       border-radius: 0.3rem;
       width: 100%;
       margin-bottom: 1rem;
+    }
+  }
+
+  button {
+    outline: none;
+    border: none;
+    background-color: #7676ff;
+    color: white;
+    font-size: 1rem;
+    padding: 0.9rem 2rem;
+    text-decoration: none;
+    border-radius: 0.3rem;
+    text-align: center;
+    font-family: "Gilroy-Bold", sans-serif;
+    cursor: pointer;
+
+    @media screen and (max-width: 600px) {
+      display: block;
+      width: 100%;
+      font-size: 1.2rem;
     }
   }
 `;
